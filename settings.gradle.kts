aa4de98fe5099c058f8ec3db0eca1c5fa93e75f0
pineapplleboy <pineapplleboy@gmail.com>
2025-06-24 02:20:36 +0700

    add snyk

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,18 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
         gradlePluginPortal()
+        google()
+        mavenCentral()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { setUrl("https://jitpack.io")  }
-        maven { setUrl("https://jitpack.io")}
     }
 }
 
 rootProject.name = "MovieCatalog"
 include(":app")
- 
